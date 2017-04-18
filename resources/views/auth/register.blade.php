dfa3e9ddd61ba762cff168a0b0715d85e93c6493
Skyler Crane <skylerc15@Skylers-MacBook-Pro.local>
2017-04-18 12:37:08 -0600

    Updated Sign-Up Page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+                <div class="panel-heading">Sign-Up</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
@@ -65,7 +65,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <ul class="actions">
-                                <li><input type="submit" value="Register" class="special" /></li>
+                                <li><input type="submit" value="Sign-Up" class="special" /></li>
                         
                             </ul>
                             </div>
